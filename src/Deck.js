2b545f2e8ccd3482345d12cd5938f9f548483a2e
AndrewNgKF <andrew.ctrl.alt.del@gmail.com>
2017-05-11 22:29:25 +0800

    added

--- src/Deck.js
+++ src/Deck.js
@@ -1,3 +1,5 @@
+// @flow
+
 import React, {Component} from 'react';
 import {
   View,
@@ -6,17 +8,49 @@ import {
 } from 'react-native';
 
 class Deck extends Component {
+  constructor(props) {
+    super(props);
+
+    const position = new Animated.ValueXY();
+
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        position.setValue({ x: gesture.dx, y: gesture.dy })
+      },
+      onPanResponderRelease: () => {}
+    });
+
+    this.state = { panResponder, position };
+  }
+  getCardStyle() {
+    return {
+      ...this.state.position.getLayout(),
+      transform: [{ rotate: '-45deg' }]
+    };
+  }
 
   renderCards() {
-    return this.props.data.map(item => {
+    return this.props.data.map((item, index) => {
+      if ( index === 0) {
+        return (
+          <Animated.View
+            key={item.id}
+            style={this.getCardStyle()}
+            {...this.state.panResponder.panHandlers}
+          >
+            {this.props.renderCard(item)}
+          </Animated.View>
+        )
+      }
       return this.props.renderCard(item);
     });
   }
   render() {
     return (
-      <View>
+      <Animated.View>
         {this.renderCards()}
-      </View>
+      </Animated.View>
     )
   }
 }
